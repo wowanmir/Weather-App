5627101769e6b11a831da894d2d6333bd543307a
Pavel <kedovpa@gmail.com>
2023-09-16 21:29:34 +0600

    Remove Console log

--- src/components/weekly-weather/weekly-weather.jsx
+++ src/components/weekly-weather/weekly-weather.jsx
@@ -17,7 +17,7 @@ export const WeeklyWeather = () => {
         console.log(error);
       });
   }, []);
-  console.log('wweeklyData', weeklyData)
+
   return (
     <div className="weekly-weather">
       {weeklyData.list.splice(0, 5).map((list) => (
